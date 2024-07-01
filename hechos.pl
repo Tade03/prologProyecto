73b7915c6c788a3e9c3339828486c5b6fcc0e927
Tade03 <tade.castillo.03@gmail.com>
2024-07-01 14:56:47 -0600

    Primer commit del proyecto

---
+++ hechos.pl
@@ -0,0 +1,583 @@
+% Region Geográfica
+region_geografica(nortenos).
+region_geografica(bajacalifornianos).
+region_geografica(bajio).
+region_geografica(occidentales).
+region_geografica(surenos).
+region_geografica(peninsulares).
+region_geografica(centroamericanos).
+
+% Etnicidad
+etnicidad(mestizos).
+etnicidad(indigenas).
+etnicidad(afrodescendientes).
+etnicidad(descendientes_de_inmigrantes).
+etnicidad(criollos).
+
+% Subcategoría de Etnicidad
+subtipo_etnicidad(nahuatl, indigenas).
+subtipo_etnicidad(maya, indigenas).
+subtipo_etnicidad(zapoteco, indigenas).
+subtipo_etnicidad(mixteco, indigenas).
+subtipo_etnicidad(otomi, indigenas).
+subtipo_etnicidad(totonaca, indigenas).
+
+subtipo_etnicidad(espanoles, descendientes_de_inmigrantes).
+subtipo_etnicidad(italianos, descendientes_de_inmigrantes).
+subtipo_etnicidad(alemanes, descendientes_de_inmigrantes).
+subtipo_etnicidad(libaneses, descendientes_de_inmigrantes).
+subtipo_etnicidad(chinos, descendientes_de_inmigrantes).
+subtipo_etnicidad(japoneses, descendientes_de_inmigrantes).
+subtipo_etnicidad(coreanos, descendientes_de_inmigrantes).
+subtipo_etnicidad(judios, descendientes_de_inmigrantes).
+subtipo_etnicidad(franceses, descendientes_de_inmigrantes).
+subtipo_etnicidad(ingleses, descendientes_de_inmigrantes).
+subtipo_etnicidad(irlandeses, descendientes_de_inmigrantes).
+subtipo_etnicidad(polacos, descendientes_de_inmigrantes).
+subtipo_etnicidad(rusos, descendientes_de_inmigrantes).
+subtipo_etnicidad(hungaros, descendientes_de_inmigrantes).
+subtipo_etnicidad(checos, descendientes_de_inmigrantes).
+subtipo_etnicidad(austriacos, descendientes_de_inmigrantes).
+subtipo_etnicidad(cubanos, descendientes_de_inmigrantes).
+subtipo_etnicidad(argentinos, descendientes_de_inmigrantes).
+subtipo_etnicidad(chilenos, descendientes_de_inmigrantes).
+subtipo_etnicidad(colombianos, descendientes_de_inmigrantes).
+subtipo_etnicidad(venezolanos, descendientes_de_inmigrantes).
+subtipo_etnicidad(peruanos, descendientes_de_inmigrantes).
+subtipo_etnicidad(brasilenos, descendientes_de_inmigrantes).
+subtipo_etnicidad(ecuatorianos, descendientes_de_inmigrantes).
+subtipo_etnicidad(bolivianos, descendientes_de_inmigrantes).
+subtipo_etnicidad(paraguayos, descendientes_de_inmigrantes).
+subtipo_etnicidad(uruguayos, descendientes_de_inmigrantes).
+subtipo_etnicidad(costarricenses, descendientes_de_inmigrantes).
+subtipo_etnicidad(salvadorenos, descendientes_de_inmigrantes).
+subtipo_etnicidad(hondurenos, descendientes_de_inmigrantes).
+subtipo_etnicidad(nicaraguenses, descendientes_de_inmigrantes).
+subtipo_etnicidad(panamenos, descendientes_de_inmigrantes).
+subtipo_etnicidad(canadienses, descendientes_de_inmigrantes).
+subtipo_etnicidad(estadounidenses, descendientes_de_inmigrantes).
+subtipo_etnicidad(filipinos, descendientes_de_inmigrantes).
+subtipo_etnicidad(indios, descendientes_de_inmigrantes).
+subtipo_etnicidad(pakistanies, descendientes_de_inmigrantes).
+subtipo_etnicidad(iranies, descendientes_de_inmigrantes).
+subtipo_etnicidad(iraquies, descendientes_de_inmigrantes).
+subtipo_etnicidad(sirios, descendientes_de_inmigrantes).
+subtipo_etnicidad(turcos, descendientes_de_inmigrantes).
+subtipo_etnicidad(griegos, descendientes_de_inmigrantes).
+subtipo_etnicidad(egipcios, descendientes_de_inmigrantes).
+subtipo_etnicidad(marroquies, descendientes_de_inmigrantes).
+subtipo_etnicidad(argelinos, descendientes_de_inmigrantes).
+subtipo_etnicidad(senegaleses, descendientes_de_inmigrantes).
+subtipo_etnicidad(sudafricanos, descendientes_de_inmigrantes).
+
+% Nivel Educativo
+nivel_educativo(analfabetos).
+nivel_educativo(educacion_basica_incompleta).
+nivel_educativo(educacion_basica_completa).
+nivel_educativo(educacion_media_superior).
+nivel_educativo(educacion_superior).
+nivel_educativo(postgrado).
+
+% Edad (1 a 120)
+edad(1).
+edad(2).
+edad(3).
+edad(4).
+edad(5).
+edad(6).
+edad(7).
+edad(8).
+edad(9).
+edad(10).
+edad(11).
+edad(12).
+edad(13).
+edad(14).
+edad(15).
+edad(16).
+edad(17).
+edad(18).
+edad(19).
+edad(20).
+edad(21).
+edad(22).
+edad(23).
+edad(24).
+edad(25).
+edad(26).
+edad(27).
+edad(28).
+edad(29).
+edad(30).
+edad(31).
+edad(32).
+edad(33).
+edad(34).
+edad(35).
+edad(36).
+edad(37).
+edad(38).
+edad(39).
+edad(40).
+edad(41).
+edad(42).
+edad(43).
+edad(44).
+edad(45).
+edad(46).
+edad(47).
+edad(48).
+edad(49).
+edad(50).
+edad(51).
+edad(52).
+edad(53).
+edad(54).
+edad(55).
+edad(56).
+edad(57).
+edad(58).
+edad(59).
+edad(60).
+edad(61).
+edad(62).
+edad(63).
+edad(64).
+edad(65).
+edad(66).
+edad(67).
+edad(68).
+edad(69).
+edad(70).
+edad(71).
+edad(72).
+edad(73).
+edad(74).
+edad(75).
+edad(76).
+edad(77).
+edad(78).
+edad(79).
+edad(80).
+edad(81).
+edad(82).
+edad(83).
+edad(84).
+edad(85).
+edad(86).
+edad(87).
+edad(88).
+edad(89).
+edad(90).
+edad(91).
+edad(92).
+edad(93).
+edad(94).
+edad(95).
+edad(96).
+edad(97).
+edad(98).
+edad(99).
+edad(100).
+edad(101).
+edad(102).
+edad(103).
+edad(104).
+edad(105).
+edad(106).
+edad(107).
+edad(108).
+edad(109).
+edad(110).
+edad(111).
+edad(112).
+edad(113).
+edad(114).
+edad(115).
+edad(116).
+edad(117).
+edad(118).
+edad(119).
+edad(120).
+
+% Etapas de Vida
+etapas_de_vida(infancia).
+etapas_de_vida(ninez).
+etapas_de_vida(adolesencia).
+etapas_de_vida(juventud).
+etapas_de_vida(adultez).
+etapas_de_vida(adulto_mayor)
+
+%Etapa_Edad
+etapa_edad(infancia, 1).
+etapa_edad(infancia, 2).
+etapa_edad(infancia, 3).
+etapa_edad(infancia, 4).
+etapa_edad(infancia, 5).
+etapa_edad(ninez, 6).
+etapa_edad(ninez, 7).
+etapa_edad(ninez, 8).
+etapa_edad(ninez, 9).
+etapa_edad(ninez, 10).
+etapa_edad(ninez, 11).
+etapa_edad(adolescencia, 12).
+etapa_edad(adolescencia, 13).
+etapa_edad(adolescencia, 14).
+etapa_edad(adolescencia, 15).
+etapa_edad(adolescencia, 16).
+etapa_edad(adolescencia, 17).
+etapa_edad(adolescencia, 18).
+etapa_edad(adolescencia, 19).
+etapa_edad(juventud, 14).
+etapa_edad(juventud, 15).
+etapa_edad(juventud, 16).
+etapa_edad(juventud, 17).
+etapa_edad(juventud, 18).
+etapa_edad(juventud, 19).
+etapa_edad(juventud, 20).
+etapa_edad(juventud, 21).
+etapa_edad(juventud, 22).
+etapa_edad(juventud, 23).
+etapa_edad(juventud, 24).
+etapa_edad(juventud, 25).
+etapa_edad(juventud, 26).
+etapa_edad(adultez, 27).
+etapa_edad(adultez, 28).
+etapa_edad(adultez, 29).
+etapa_edad(adultez, 30).
+etapa_edad(adultez, 31).
+etapa_edad(adultez, 32).
+etapa_edad(adultez, 33).
+etapa_edad(adultez, 34).
+etapa_edad(adultez, 35).
+etapa_edad(adultez, 36).
+etapa_edad(adultez, 37).
+etapa_edad(adultez, 38).
+etapa_edad(adultez, 39).
+etapa_edad(adultez, 40).
+etapa_edad(adultez, 41).
+etapa_edad(adultez, 42).
+etapa_edad(adultez, 43).
+etapa_edad(adultez, 44).
+etapa_edad(adultez, 45).
+etapa_edad(adultez, 46).
+etapa_edad(adultez, 47).
+etapa_edad(adultez, 48).
+etapa_edad(adultez, 49).
+etapa_edad(adultez, 50).
+etapa_edad(adultez, 51).
+etapa_edad(adultez, 52).
+etapa_edad(adultez, 53).
+etapa_edad(adultez, 54).
+etapa_edad(adultez, 55).
+etapa_edad(adultez, 56).
+etapa_edad(adultez, 57).
+etapa_edad(adultez, 58).
+etapa_edad(adultez, 59).
+etapa_edad(adulto_mayor, 60).
+etapa_edad(adulto_mayor, 61).
+etapa_edad(adulto_mayor, 62).
+etapa_edad(adulto_mayor, 63).
+etapa_edad(adulto_mayor, 64).
+etapa_edad(adulto_mayor, 65).
+etapa_edad(adulto_mayor, 66).
+etapa_edad(adulto_mayor, 67).
+etapa_edad(adulto_mayor, 68).
+etapa_edad(adulto_mayor, 69).
+etapa_edad(adulto_mayor, 70).
+etapa_edad(adulto_mayor, 71).
+etapa_edad(adulto_mayor, 72).
+etapa_edad(adulto_mayor, 73).
+etapa_edad(adulto_mayor, 74).
+etapa_edad(adulto_mayor, 75).
+etapa_edad(adulto_mayor, 76).
+etapa_edad(adulto_mayor, 77).
+etapa_edad(adulto_mayor, 78).
+etapa_edad(adulto_mayor, 79).
+etapa_edad(adulto_mayor, 80).
+etapa_edad(adulto_mayor, 81).
+etapa_edad(adulto_mayor, 82).
+etapa_edad(adulto_mayor, 83).
+etapa_edad(adulto_mayor, 84).
+etapa_edad(adulto_mayor, 85).
+etapa_edad(adulto_mayor, 86).
+etapa_edad(adulto_mayor, 87).
+etapa_edad(adulto_mayor, 88).
+etapa_edad(adulto_mayor, 89).
+etapa_edad(adulto_mayor, 90).
+etapa_edad(adulto_mayor, 91).
+etapa_edad(adulto_mayor, 92).
+etapa_edad(adulto_mayor, 93).
+etapa_edad(adulto_mayor, 94).
+etapa_edad(adulto_mayor, 95).
+etapa_edad(adulto_mayor, 96).
+etapa_edad(adulto_mayor, 97).
+etapa_edad(adulto_mayor, 98).
+etapa_edad(adulto_mayor, 99).
+etapa_edad(adulto_mayor, 100).
+etapa_edad(adulto_mayor, 101).
+etapa_edad(adulto_mayor, 102).
+etapa_edad(adulto_mayor, 103).
+etapa_edad(adulto_mayor, 104).
+etapa_edad(adulto_mayor, 105).
+etapa_edad(adulto_mayor, 106).
+etapa_edad(adulto_mayor, 107).
+etapa_edad(adulto_mayor, 108).
+etapa_edad(adulto_mayor, 109).
+etapa_edad(adulto_mayor, 110).
+etapa_edad(adulto_mayor, 111).
+etapa_edad(adulto_mayor, 112).
+etapa_edad(adulto_mayor, 113).
+etapa_edad(adulto_mayor, 114).
+etapa_edad(adulto_mayor, 115).
+etapa_edad(adulto_mayor, 116).
+etapa_edad(adulto_mayor, 117).
+etapa_edad(adulto_mayor, 118).
+etapa_edad(adulto_mayor, 119).
+etapa_edad(adulto_mayor, 120).
+
+% Situación Económica
+situacion_economica(clase_baja).
+situacion_economica(clase_media_baja).
+situacion_economica(clase_media).
+situacion_economica(clase_media_alta).
+situacion_economica(clase_alta).
+
+% Región Geográfica
+region_geografica(nortenos).
+region_geografica(bajacalifornianos).
+region_geografica(bajio).
+region_geografica(occidentales).
+region_geografica(surenos).
+region_geografica(peninsulares).
+region_geografica(centroamericanos).
+
+% Etnias
+etnicidad(mestizos).
+etnicidad(indigenas).
+etnicidad(afrodescendientes).
+etnicidad(descendientes_de_inmigrantes).
+etnicidad(criollos).
+
+% Nivel Educativo
+nivel_educativo(analfabetos).
+nivel_educativo(educacion_basica_incompleta).
+nivel_educativo(educacion_basica_completa).
+nivel_educativo(educacion_media_superior).
+nivel_educativo(educacion_superior).
+nivel_educativo(postgrado).
+
+% Religión
+religion(catolicos).
+religion(protestantes_evangelicos).
+religion(testigos_de_jehova).
+religion(mormones).
+religion(judios).
+religion(musulmanes).
+religion(budistas).
+religion(hinduistas).
+religion(sin_religion).
+
+% Idioma
+idioma(hablantes_de_espanol).
+idioma(hablantes_de_lenguas_indigenas).
+idioma(hablantes_de_lenguas_extranjeras).
+
+% Subcategoria de Idioma
+
+subtipo_idioma(nahuatl, hablantes_de_lenguas_indigenas).
+subtipo_idioma(maya, hablantes_de_lenguas_indigenas).
+subtipo_idioma(zapoteco, hablantes_de_lenguas_indigenas).
+subtipo_idioma(ingles, hablantes_de_lenguas_extranjeras).
+subtipo_idioma(aleman, hablantes_de_lenguas_extranjeras).
+subtipo_idioma(frances, hablantes_de_lenguas_extranjeras).
+
+% Ocupación
+ocupacion(campesinos).
+ocupacion(pescadores).
+ocupacion(mineros).
+ocupacion(obrero_industriales).
+ocupacion(comerciantes).
+ocupacion(profesionales).
+ocupacion(funcionarios_publicos).
+ocupacion(artistas_y_artesanos).
+ocupacion(trabajadores_del_sector_servicios).
+ocupacion(emprendedores).
+
+% Estado Civil
+estado_civil(solteros).
+estado_civil(casados).
+estado_civil(divorciados).
+estado_civil(viudos).
+estado_civil(en_union_libre).
+
+% Géneros
+genero(hombre).
+genero(mujer).
+genero(no_binaria).
+genero(transgenero).
+genero(agenero).
+genero(bigenero).
+genero(fluido).
+
+% Residencia
+residencia(urbanos).
+residencia(suburbanos).
+residencia(rurales).
+residencia(migrantes).
+
+% Participación Política
+participacion_politica(activos_politicamente).
+participacion_politica(no_activos_politicamente).
+participacion_politica(militantes_de_partidos_politicos).
+participacion_politica(independientes).
+
+% Condición de Salud
+condicion_de_salud(personas_con_discapacidad).
+condicion_de_salud(personas_sin_discapacidad).
+condicion_de_salud(personas_con_enfermedades_cronicas).
+condicion_de_salud(personas_con_enfermedades_mentales).
+
+% Preferencia Alimentaria
+preferencia_alimentaria(omnivoros).
+preferencia_alimentaria(vegetarianos).
+preferencia_alimentaria(veganos).
+preferencia_alimentaria(pescetarianos).
+preferencia_alimentaria(flexitarianos).
+
+% Actividad Física
+actividad_fisica(activos_fisicamente).
+actividad_fisica(sedentarios).
+actividad_fisica(deportistas_profesionales).
+actividad_fisica(aficionados_al_fitness).
+
+% Lugar actividad fisica
+
+lugar_actividad_fisica(activos_fisicamente, al_aire_libre).
+lugar_actividad_fisica(deportistas_profesionales, instalaciones_deportivas).
+
+% Tiempo a la actividad fisica
+
+tiempo_dedicado_actividad_fisica(aficionados_al_fitness, alta).
+tiempo_dedicado_actividad_fisica(sedentarios, baja).
+
+% Tiempo preferido 
+
+tiempo_contenido_pregerido(consumidores_de_television, series_y_peliculas).
+tiempo_contenido_preferido(lectores_de_periodicos_y_revistas).
+
+
+% Consumo de Medios
+consumo_de_medios(consumidores_de_television).
+consumo_de_medios(usuarios_de_redes_sociales).
+consumo_de_medios(lectores_de_periodicos_y_revistas).
+consumo_de_medios(radioescuchas).
+consumo_de_medios(consumidores_de_contenido_digital).
+
+% Relación entre Situación Económica y Religión
+situacion_religion(clase_media, catolicos).
+situacion_religion(clase_media_alta, judios).
+situacion_religion(clase_alta, protestantes_evangelicos).
+situacion_religion(clase_media_baja, testigos_de_jehova).
+situacion_religion(clase_media, mormones).
+
+% Relación entre Situación Económica y Idioma
+situacion_idioma(clase_media, hablantes_de_espanol).
+situacion_idioma(clase_media_alta, hablantes_de_lenguas_extranjeras).
+situacion_idioma(clase_baja, hablantes_de_lenguas_indigenas).
+situacion_idioma(clase_alta, hablantes_de_lenguas_extranjeras).
+
+% Relación entre Religión y Idioma
+religion_idioma(catolicos, hablantes_de_espanol).
+religion_idioma(judios, hablantes_de_lenguas_extranjeras).
+religion_idioma(musulmanes, hablantes_de_lenguas_extranjeras).
+religion_idioma(budistas, hablantes_de_lenguas_extranjeras).
+religion_idioma(hinduistas, hablantes_de_lenguas_extranjeras).
+
+% Relación entre Subtipo de Idioma y Situación Económica
+subtipo_situacion(nahuatl, clase_baja).
+subtipo_situacion(maya, clase_baja).
+subtipo_situacion(zapoteco, clase_media_baja).
+subtipo_situacion(ingles, clase_media_alta).
+subtipo_situacion(frances, clase_media_alta).
+
+% Relación entre Subtipo de Idioma y Religión
+subtipo_religion(nahuatl, sin_religion).
+subtipo_religion(ingles, protestantes_evangelicos).
+subtipo_religion(aleman, catolicos).
+subtipo_religion(frances, judios).
+subtipo_religion(maya, catolicos).
+
+% ---
+
+dispositivo_uso_medio(usuarios_de_redes_sociales, smartphones).
+dispositivo_uso_medio(consumidores_de_contenido_digital, computadoras_y_tabletas).
+
+% Preferencia Alimentaria y Actividad Física
+preferencia_actividad(vegetarianos, actividad_fisica, baja).
+preferencia_actividad(veganos, actividad_fisica, alta).
+preferencia_actividad(pescetarianos, actividad_fisica, media).
+preferencia_actividad(flexitarianos, actividad_fisica, variable).
+
+% Lugar y Tiempo de Actividad Física
+lugar_tiempo_actividad(activos_fisicamente, al_aire_libre, tarde).
+lugar_tiempo_actividad(deportistas_profesionales, instalaciones_deportivas, manana).
+lugar_tiempo_actividad(aficionados_al_fitness, gimnasios, noche).
+lugar_tiempo_actividad(sedentarios, casa, indiferente).
+
+% Consumo de Medios y Preferencia de Contenido
+medio_preferencia_contenido(consumidores_de_television, entretenimiento, series_y_peliculas).
+medio_preferencia_contenido(usuarios_de_redes_sociales, informacion, noticias).
+medio_preferencia_contenido(lectores_de_periodicos_y_revistas, educacion, articulos_de_opinion).
+
+% Relación entre Actividad Física y Consumo de Medios
+actividad_consumo_medios(activos_fisicamente, bajo, redes_sociales).
+actividad_consumo_medios(sedentarios, alto, television).
+actividad_consumo_medios(deportistas_profesionales, medio, revistas_especializadas).
+actividad_consumo_medios(aficionados_al_fitness, medio, blogs_de_fitness).
+
+% Preferencia Alimentaria, Actividad Física y Edad
+preferencia_actividad_edad(vegetarianos, actividad_fisica, baja, adultos_jovenes).
+preferencia_actividad_edad(veganos, actividad_fisica, alta, adultos).
+preferencia_actividad_edad(pescetarianos, actividad_fisica, media, adultos_mayores).
+preferencia_actividad_edad(flexitarianos, actividad_fisica, variable, adolescentes).
+
+% Lugar, Tiempo de Actividad Física y Género
+lugar_tiempo_actividad_genero(activos_fisicamente, al_aire_libre, tarde, femenino).
+lugar_tiempo_actividad_genero(deportistas_profesionales, instalaciones_deportivas, manana, masculino).
+lugar_tiempo_actividad_genero(aficionados_al_fitness, gimnasios, noche, femenino).
+lugar_tiempo_actividad_genero(sedentarios, casa, indiferente, indistinto).
+
+% Consumo de Medios, Preferencia de Contenido y Edad
+medio_preferencia_contenido_edad(consumidores_de_television, entretenimiento, series_y_peliculas, adultos).
+medio_preferencia_contenido_edad(usuarios_de_redes_sociales, informacion, noticias, jovenes_adultos).
+medio_preferencia_contenido_edad(lectores_de_periodicos_y_revistas, educacion, articulos_de_opinion, adultos_mayores).
+
+% Relación entre Actividad Física, Consumo de Medios, y Género
+actividad_consumo_medios_genero(activos_fisicamente, bajo, redes_sociales, masculino).
+actividad_consumo_medios_genero(sedentarios, alto, television, femenino).
+actividad_consumo_medios_genero(deportistas_profesionales, medio, revistas_especializadas, masculino).
+actividad_consumo_medios_genero(aficionados_al_fitness, medio, blogs_de_fitness, femenino).
+
+% perfil_demografico
+
+perfil_demografico(bajio, mestizos, educacion_media_superior, jovenes_adultos, clase_media).
+perfil_demografico(occidentales, indigenas, educacion_basica_incompleta, ninos, clase_baja).
+perfil_demografico(surenos, afrodescendientes, educacion_superior, adultos, clase_media_baja).
+perfil_demografico(peninsulares, descendientes_de_inmigrantes, postgrado, adultos_mayores, clase_media).
+perfil_demografico(centroamericanos, criollos, educacion_basica_completa, adolescentes, clase_media_baja).
+perfil_demografico(bajio, indigenas, analfabetos, adultos_mayores, clase_baja).
+perfil_demografico(occidentales, mestizos, educacion_media_superior, adultos, clase_media).
+perfil_demografico(surenos, criollos, educacion_superior, jovenes_adultos, clase_media).
+perfil_demografico(peninsulares, afrodescendientes, educacion_basica_incompleta, ninos, clase_baja).
+perfil_demografico(centroamericanos, mestizos, postgrado, adultos, clase_media).
+perfil_demografico(bajio, indigenas, educacion_basica_completa, adolescentes, clase_media_baja).
+perfil_demografico(occidentales, descendientes_de_inmigrantes, educacion_media_superior, jovenes_adultos, clase_media).
+perfil_demografico(surenos, criollos, analfabetos, adultos_mayores, clase_baja).
+perfil_demografico(peninsulares, mestizos, educacion_superior, adultos, clase_media_alta).
+perfil_demografico(centroamericanos, afrodescendientes, educacion_basica_incompleta, ninos, clase_baja).
+perfil_demografico(bajio, criollos, postgrado, adultos_mayores, clase_media).
+perfil_demografico(occidentales, indigenas, educacion_media_superior, adultos, clase_media_baja).
+perfil_demografico(surenos, mestizos, educacion_superior, jovenes_adultos, clase_media).
+perfil_demografico(peninsulares, descendientes_de_inmigrantes, educacion_basica_completa, adolescentes, clase_media_baja).
+perfil_demografico(centroamericanos, indigenas, analfabetos, adultos_mayores, clase_baja).
